5ea6e896bfa1ce31191cb458a6ac226ec5e9528a
Rdio <neyamina2b@gmail.com>
2024-05-01 14:05:59 -0600

    Segunda version con while

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int i = 0;
         int añoDeLanzamiento = 1999;
 
-        double mediaEvaluacionIngresada = 0;
+//        double mediaEvaluacionIngresada = 0;
         double evaluacion = 4;
         double mediaEvaluacion = (4.5 + 2.3 + 5) / 3;
+//        double notaPelicula = 0;
+        double evaluacionUsuario = 0;
+        double mediaCalculadaPorEvaluacionDeUsuario = 0;
 
         boolean incluidoEnElPlanBásico = true;
 
@@ -28,12 +34,20 @@ public class Main {
         } else {
             System.out.println("Película retro");
         }
-        for (int i = 0; i < 3; i++) {
-            Scanner teclado = new Scanner(System.in);
-            System.out.println("Evalue la pelicula: ");
-            double notaPelicula = teclado.nextDouble();
-            mediaEvaluacionIngresada += notaPelicula;
+        while (i < 3) {
+            System.out.println("Evalua la pelicula: ");
+            evaluacionUsuario = teclado.nextDouble();
+            mediaCalculadaPorEvaluacionDeUsuario += evaluacionUsuario;
+            i++;
         }
-        System.out.println("Evaluacion hecha por el usuario: " + mediaEvaluacionIngresada / 3);
+        System.out.println("Evaluacion hecha por el usuario: " + mediaCalculadaPorEvaluacionDeUsuario / 3);
+//        for (int i = 0; i < 3; i++) {
+//            Scanner teclado = new Scanner(System.in);
+//            System.out.println("Evalue la pelicula: ");
+//            notaPelicula= teclado.nextDouble();
+//            mediaEvaluacionIngresada += notaPelicula;
+//        }
+//        System.out.println("Evaluacion hecha por el usuario: " + mediaEvaluacionIngresada / 3);
+//        }
     }
 }
